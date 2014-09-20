d8a4e54f9b7145f61d771eda4a2b5438323010e0
Ryan Scott <atthealma@gmail.com>
2014-09-20 23:50:36 +1000

    Better example using phaser.io.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,19 @@
 //= require_tree ./game
+
+window.gameRuntime = (function() {
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-stage', { preload: preload, create: create, update: update });
+
+  function preload() {
+    game.load.image('renderTest', 'assets/mario.png');
+  }
+
+  function create() {
+    game.add.sprite(400 - 32, 300 - 32, 'renderTest');
+  }
+
+  function update() {
+      
+  }
+
+  return game;
+})();
