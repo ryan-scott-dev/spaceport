c5cbc5042c06f388846c36bab2dddd1dd0b9b514
Ryan Scott <atthealma@gmail.com>
2014-09-21 00:25:44 +1000

    Started creating basic tilemap controls.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,19 +1,49 @@
 //= require_tree ./game
 
 window.gameRuntime = (function() {
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-stage', { preload: preload, create: create, update: update });
-
   function preload() {
-    game.load.image('renderTest', 'assets/mario.png');
+    game.load.image('tiles', 'assets/tmw_desert_spacing.png');
   }
 
   function create() {
-    game.add.sprite(400 - 32, 300 - 32, 'renderTest');
+    game.stage.backgroundColor = '#2d2d2d';
+
+    map = game.add.tilemap();
+    map.addTilesetImage('tiles');
+
+    layer = map.create('Ground', 40, 30, 32, 32);
+
+    layer.resizeWorld();
+
+    marker = game.add.graphics();
+    marker.lineStyle(2, 0xFFFFFF, 1);
+    marker.drawRect(0, 0, 32, 32);
+
+    cursors = game.input.keyboard.createCursorKeys();
   }
 
   function update() {
-      
+    marker.x = layer.getTileX(game.input.activePointer.worldX) * 32;
+    marker.y = layer.getTileY(game.input.activePointer.worldY) * 32;
+
+    if (cursors.left.isDown) {
+      game.camera.x -= 4;
+    } else if (cursors.right.isDown) {
+      game.camera.x += 4;
+    }
+
+    if (cursors.up.isDown) {
+      game.camera.y -= 4;
+    } else if (cursors.down.isDown) {
+      game.camera.y += 4;
+    }
   }
+
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-stage', { 
+    preload: preload, 
+    create: create, 
+    update: update 
+  });
 
   return game;
 })();
