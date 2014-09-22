cf0735b7daf183a0edec1599620852e208dca4c8
Ryan Scott <atthealma@gmail.com>
2014-09-23 08:15:24 +1000

    Gave buildings a placing and a placed state.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -57,48 +57,59 @@ window.gameRuntime = (function() {
     state.buildings = state.buildings || [];
 
     state.buildings.forEach(function(building) {
-      switch (building.type) {
-        case 'wall': {
-          var wall = createWallSilhouette(building.x, building.y, building.rotation);
-          this.buildings.push(wall);
-          break;
-        }
-        case 'orbital': {
-          var orbital = createOrbitalSilhouette(building.x, building.y, building.rotation);
-          this.buildings.push(orbital);
-          break;
-        }
-        case 'door': {
-          var door = createDoorSilhouette(building.x, building.y, building.rotation);
-          this.buildings.push(door);
-          break;
-        }
-        default: {
-          console.error("Unable to create building of type '" + building.type + "'");
-        }
-      }
+      addBuilding(building);
     });
-  }
+  };
 
-  function createOrbitalSilhouette(x, y, rotation) {
+  function addBuilding(building) {
+    switch (building.type) {
+      case 'wall': {
+        var wall = createWallSilhouette(building.x, building.y, building.rotation, true);
+        this.buildings.push(wall);
+        break;
+      }
+      case 'orbital': {
+        var orbital = createOrbitalSilhouette(building.x, building.y, building.rotation, true);
+        this.buildings.push(orbital);
+        break;
+      }
+      case 'door': {
+        var door = createDoorSilhouette(building.x, building.y, building.rotation, true);
+        this.buildings.push(door);
+        break;
+      }
+      default: {
+        console.error("Unable to create building of type '" + building.type + "'");
+      }
+    }
+  };
+
+  function createOrbitalSilhouette(x, y, rotation, placed) {
     var orbitalGroup = game.add.group();
     orbitalGroup.x = x || 0;
     orbitalGroup.y = y || 0;
     orbitalGroup.rotation = rotation || 0;
     orbitalGroup.pivot.x = (32 * 3) / 2;
     orbitalGroup.pivot.y = (32 * 5) / 2;
+    orbitalGroup.type = 'orbital';
 
     var orbital = game.add.graphics(0, 0, orbitalGroup);
     orbital.lineStyle(2, 0xFF00FF, 1);
     orbital.drawRect(0, 0, 32 * 3, 32 * 4);
-
-    var orbitalLoadZone = game.add.graphics(0, 128, orbitalGroup);
-    orbitalLoadZone.lineStyle(2, 0x00FF00, 1);
-    orbitalLoadZone.drawRect(0, 0, 32 * 3, 32);
-
+    
+    if (placed) {
+      orbital.beginFill(0xFF00FF, 0.5);
+      orbital.drawRect(0, 0, 32 * 3, 32 * 4);
+      orbital.endFill();
+    } else {
+      var orbitalLoadZone = game.add.graphics(0, 128, orbitalGroup);
+      orbitalLoadZone.lineStyle(1, 0x00FF00, 1);
+      orbitalLoadZone.drawRect(0, 0, 32 * 3, 32);  
+    }
+    
     orbitalGroup.serialize = function() {
       return {
-        type: 'orbital',
+        type: this.type,
         x: this.x,
         y: this.y,
         rotation: this.rotation,
@@ -108,20 +119,22 @@ window.gameRuntime = (function() {
     return orbitalGroup;
   }
 
-  function createWallSilhouette(x, y, rotation) {
+  function createWallSilhouette(x, y, rotation, placed) {
     var wall = game.add.graphics();
     wall.x = x || 0;
     wall.y = y || 0;
     wall.rotation = rotation || 0;
+    wall.type = 'wall';
     wall.pivot.x = (32 * 1) / 2;
     wall.pivot.y = 16;
+    
     wall.lineStyle(2, 0xFF1D3D, 1);
     wall.moveTo(0, 0);
     wall.lineTo(32 * 1, 0);
 
     wall.serialize = function() {
       return {
-        type: 'wall',
+        type: this.type,
         x: this.x,
         y: this.y,
         rotation: this.rotation,
@@ -131,27 +144,40 @@ window.gameRuntime = (function() {
     return wall;
   }
 
-  function createDoorSilhouette(x, y, rotation) {
-    var door = game.add.graphics();
-    door.x = x || 0;
-    door.y = y || 0;
-    door.rotation = rotation || 0;
-    door.pivot.x = (32 * 3) / 2;
-    door.pivot.y = 16;
+  function createDoorSilhouette(x, y, rotation, placed) {
+    var doorGroup = game.add.group();
+    doorGroup.x = x || 0;
+    doorGroup.y = y || 0;
+    doorGroup.rotation = rotation || 0;
+    doorGroup.type = 'door';
+    doorGroup.pivot.x = (32 * 3) / 2;
+    doorGroup.pivot.y = 16;
+
+    if (!placed) {
+      var doorTriggerZone1 = game.add.graphics(0, -32, doorGroup);
+      doorTriggerZone1.lineStyle(1, 0x00FF00, 1);
+      doorTriggerZone1.drawRect(0, 0, 32 * 3, 32);
+
+      var doorTriggerZone2 = game.add.graphics(0, 0, doorGroup);
+      doorTriggerZone2.lineStyle(1, 0x00FF00, 1);
+      doorTriggerZone2.drawRect(0, 0, 32 * 3, 32);
+    }
+
+    var door = game.add.graphics(0, 0, doorGroup);
     door.lineStyle(2, 0x1D3DFF, 1);
     door.moveTo(0, 0);
     door.lineTo(32 * 3, 0);
 
-    door.serialize = function() {
+    doorGroup.serialize = function() {
       return {
-        type: 'door',
+        type: this.type,
         x: this.x,
         y: this.y,
         rotation: this.rotation,
       };
     };
 
-    return door;
+    return doorGroup;
   }
 
   function update() {
@@ -170,13 +196,13 @@ window.gameRuntime = (function() {
 
     if (actions.place.isDown && actions.place.repeats == 0 && placing) {
       console.log('Placed Building');
-      
-      buildings.push(placing);
+      addBuilding(placing);
       save();
       /* TODO - Register building created */
       /* TODO - Make Sound Effect */
       /* TODO - Remove silhouette and add 'proper' implementation */
 
+      placing.destroy();
       placing = null;
     }
 
