211ad8f9fab00774a3c9168a5f4d934e6281acb8
Ryan Scott <atthealma@gmail.com>
2014-09-22 19:33:41 +1000

    Added some other structures.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -25,7 +25,11 @@ window.gameRuntime = (function() {
     actions = {
       rotate: game.input.keyboard.addKey(Phaser.Keyboard.R),
       place_orbital: game.input.keyboard.addKey(Phaser.Keyboard.O),
+      place_wall: game.input.keyboard.addKey(Phaser.Keyboard.W),
+      place_door: game.input.keyboard.addKey(Phaser.Keyboard.D),
+
       cancel: game.input.keyboard.addKey(Phaser.Keyboard.ESC),
+      place: game.input.keyboard.addKey(Phaser.Keyboard.ENTER),
     };
   }
 
@@ -45,6 +49,26 @@ window.gameRuntime = (function() {
     return orbitalGroup;
   }
 
+  function createWallSilhouette() {
+    wall = game.add.graphics();
+    wall.pivot.x = (32 * 1) / 2;
+    wall.pivot.y = (32 * 1) / 2;
+    wall.lineStyle(2, 0xFF1D3D, 1);
+    wall.drawRect(0, 0, 32 * 1, 32 * 1);
+
+    return wall;
+  }
+
+  function createDoorSilhouette() {
+    door = game.add.graphics();
+    door.pivot.x = (32 * 1) - 16;
+    door.pivot.y = (32 * 1) / 2;
+    door.lineStyle(2, 0x1D3DFF, 1);
+    door.drawRect(0, 0, 32 * 2, 32 * 1);
+
+    return door;
+  }
+
   function update() {
     marker.x = layer.getTileX(game.input.activePointer.worldX) * 32;
     marker.y = layer.getTileY(game.input.activePointer.worldY) * 32;
@@ -59,9 +83,29 @@ window.gameRuntime = (function() {
       placing.rotation += -Math.PI / 2;
     }
 
+    if (actions.place.isDown && actions.place.repeats == 0 && placing) {
+      console.log('Placed Building');
+      
+      /* TODO - Register building created */
+      /* TODO - Make Sound Effect */
+      /* TODO - Remove silhouette and add 'proper' implementation */
+
+      placing = null;
+    }
+
     if (actions.place_orbital.isDown && actions.place_orbital.repeats == 0 && !placing) {
       console.log('Placing Orbital Dock');
       placing = createDockSilhouette();
+    }
+
+    if (actions.place_wall.isDown && actions.place_wall.repeats == 0 && !placing) {
+      console.log('Placing Wall');
+      placing = createWallSilhouette();
+    }
+
+    if (actions.place_door.isDown && actions.place_door.repeats == 0 && !placing) {
+      console.log('Placing Door');
+      placing = createDoorSilhouette();
     }
 
     if (actions.cancel.isDown && actions.cancel.repeats == 0) {
